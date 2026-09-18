9a53083efebf33541d0531153a52d10b4fdec112
bebrikosik <amalozyomov@inbox.ru>
2026-09-18 14:07:05 +0300

    Add files via upload

---
+++ laba1_task3/task3.java
@@ -0,0 +1,9 @@
+public class task3 {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 5; x++){
+            for (int y = 1; y <= 5; y++){
+                System.out.println(x + " * " + y + " = " + (x * y) + "\n");
+            }
+        }
+    }
+}
